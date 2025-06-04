b88b7f0274254b1ad7f33bd3b143328ed07deecf
Lokanatha Vighnesh Achutha <lokanathavighnesh@gmail.com>
2025-06-04 08:26:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.7 MB (99.98%)

---
+++ solutions/0027-remove-element/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i] != val){
+                arr.add(nums[i]);
+                count++;
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            nums[i] = arr.get(i);
+        }
+        return count;
+ 
+    }
+}
