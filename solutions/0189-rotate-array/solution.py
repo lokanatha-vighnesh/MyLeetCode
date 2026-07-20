51c65c85387b733a7feec03999f31fa6f3834564
Lokanatha Vighnesh Achutha <lokanathavighnesh@gmail.com>
2026-07-20 15:35:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.4 MB (29.57%)

---
+++ solutions/0189-rotate-array/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nums[:] = nums[-(k%len(nums)):] + nums[:-(k%len(nums))]
+        return nums
