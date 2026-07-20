76779c4bbda340fcb87f80219ea419ac2c58b906
Lokanatha Vighnesh Achutha <lokanathavighnesh@gmail.com>
2026-07-20 18:01:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (54.97%)

---
+++ solutions/1115-valid-boomerang/solution.py
@@ -0,0 +1,3 @@
+class Solution(object):
+    def isBoomerang(self, points):
+        return True if (0.5)*abs(points[0][0]*(points[1][1]-points[2][1])+points[1][0]*(points[2][1]-points[0][1])+points[2][0]*(points[0][1]-points[1][1])) > 0 else False
